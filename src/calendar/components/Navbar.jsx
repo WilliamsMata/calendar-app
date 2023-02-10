5f14885a2963ea10254db3bf1b5fea8f5096709e
Williams Mata <williams.rm99@gmail.com>
2023-02-10 15:15:46 -0400

    toolbar centered

--- src/calendar/components/Navbar.jsx
+++ src/calendar/components/Navbar.jsx
@@ -9,6 +9,7 @@ export const Navbar = () => {
         <img src="calendar.svg" alt="calendar image" className="h-8 w-8" />
         <p className="px-2 text-xl normal-case">Williams</p>
       </div>
+
       <div className="flex gap-4">
         <button className="btn-circle btn" onClick={switchMode}>
           {darkMode ? (
@@ -17,6 +18,7 @@ export const Navbar = () => {
             <img src="moon.svg" alt="dark mode" className="h-6 w-6" />
           )}
         </button>
+
         <button id="logout-btn" className="btn-outline btn-error btn gap-2">
           <img src="logout.svg" alt="logout btn" className="h-6 w-6" />
           logout
