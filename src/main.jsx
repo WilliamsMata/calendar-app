f14eee4dacae85558b8a6cc2877947e64286b7a4
Williams Mata <williams.rm99@gmail.com>
2023-02-11 19:32:35 -0400

    redux store configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+
+//styles
 import "./styles/calendar.css";
 import "./styles/modal.css";
 import "sweetalert2/dist/sweetalert2.min.css";
 import "./index.css";
-import { router } from "./router/AppRouter";
+
+import { router } from "./router";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/ui/uiSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const uiSlice = createSlice({
+  name: "ui",
+  initialState: {
+    isDateModalOpen: false,
+  },
+  reducers: {
+    onOpenDateModal: (state) => {
+      state.isDateModalOpen = true;
+    },
+    onCloseDateModal: (state) => {
+      state.isDateModalOpen = false;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { onOpenDateModal, onCloseDateModal } = uiSlice.actions;
