f67c90348cdfc9dd5329127e59d6d6d5111ce34d
Williams Mata <williams.rm99@gmail.com>
2023-02-12 00:40:11 -0400

    fixed date error

--- src/store/store.js
+++ src/store/store.js
@@ -6,4 +6,8 @@ export const store = configureStore({
     ui: uiSlice.reducer,
     calendar: calendarSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
