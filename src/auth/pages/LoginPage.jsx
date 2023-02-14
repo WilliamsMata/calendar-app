b705a57276e87ad064432df56cef9622e38972e6
Williams Mata <williams.rm99@gmail.com>
2023-02-14 19:27:08 -0400

    login and sign up useForm configured

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,31 +1,45 @@
 import { Link } from "react-router-dom";
 import { getLoginMessageEN, getLoginMessageES } from "../../helpers";
-import { useLanguage } from "../../hooks";
+import { useForm, useLanguage } from "../../hooks";
+
+const loginFormFields = {
+  loginEmail: "",
+  loginPassword: "",
+};
 
 export const LoginPage = () => {
   const { isSpanish } = useLanguage();
-
   const { title, email, password, button, text, link } = isSpanish
     ? getLoginMessageES()
     : getLoginMessageEN();
 
+  const { loginEmail, loginPassword, onInputChange } = useForm(loginFormFields);
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    console.log({ loginEmail, loginPassword });
+  };
+
   return (
     <>
       <h1 className="font-bold tracking-tight">{title}</h1>
-      <form className="space-y-4 md:space-y-6" action="#">
+      <form className="space-y-4 md:space-y-6" onSubmit={loginSubmit}>
         <div>
           <label htmlFor="email" className="mb-2 block text-sm font-medium">
             {email}
           </label>
           <input
             type="email"
-            name="email"
             id="email"
+            name="loginEmail"
+            value={loginEmail}
+            onChange={onInputChange}
             className="input-bordered input-info input w-full"
             placeholder="name@company.com"
             required
           />
         </div>
+
         <div>
           <label
             htmlFor="password"
@@ -35,19 +49,23 @@ export const LoginPage = () => {
           </label>
           <input
             type="password"
-            name="password"
             id="password"
+            name="loginPassword"
+            value={loginPassword}
+            onChange={onInputChange}
             placeholder="••••••••"
             className="input-bordered input-info input w-full"
             required
           />
         </div>
+
         <button
           type="submit"
           className="btn-info btn w-full transition hover:brightness-110"
         >
           {button}
         </button>
+
         <p className="text-sm font-light">
           {text}
           <Link to="/auth/signup" className="link-info link font-bold">
--- src/auth/pages/SignUpPage.jsx
+++ src/auth/pages/SignUpPage.jsx
@@ -1,44 +1,73 @@
 import { Link } from "react-router-dom";
 import { getSignUpMessageEN, getSingUpMessageES } from "../../helpers";
-import { useLanguage } from "../../hooks";
+import { useForm, useLanguage } from "../../hooks";
+
+const registerFormFields = {
+  registerName: "",
+  registerEmail: "",
+  registerPassword: "",
+  registerPassword2: "",
+};
 
 export const SignUpPage = () => {
   const { isSpanish } = useLanguage();
+  const { title, name, email, password, repeatPassword, button, text, link } =
+    isSpanish ? getSingUpMessageES() : getSignUpMessageEN();
 
-  const { title, name, email, password, button, text, link } = isSpanish
-    ? getSingUpMessageES()
-    : getSignUpMessageEN();
+  const {
+    registerName,
+    registerEmail,
+    registerPassword,
+    registerPassword2,
+    onInputChange,
+  } = useForm(registerFormFields);
+
+  const registerSubmit = (event) => {
+    event.preventDefault();
+    console.log({
+      registerName,
+      registerEmail,
+      registerPassword,
+      registerPassword2,
+    });
+  };
 
   return (
     <>
       <h1 className="font-bold tracking-tight">{title}</h1>
-      <form className="space-y-4 md:space-y-6" action="#">
+      <form className="space-y-4 md:space-y-6" onSubmit={registerSubmit}>
         <div>
           <label htmlFor="name" className="mb-2 block text-sm font-medium  ">
             {name}
           </label>
           <input
             type="text"
-            name="name"
             id="name"
+            name="registerName"
+            value={registerName}
+            onChange={onInputChange}
             className="input-bordered input-info input w-full"
             placeholder={name}
             required
           />
         </div>
+
         <div>
           <label htmlFor="email" className="mb-2 block text-sm font-medium  ">
             {email}
           </label>
           <input
             type="email"
-            name="email"
             id="email"
+            name="registerEmail"
+            value={registerEmail}
+            onChange={onInputChange}
             className="input-bordered input-info input w-full"
             placeholder="name@company.com"
             required
           />
         </div>
+
         <div>
           <label
             htmlFor="password"
@@ -48,13 +77,35 @@ export const SignUpPage = () => {
           </label>
           <input
             type="password"
-            name="password"
             id="password"
+            name="registerPassword"
+            value={registerPassword}
+            onChange={onInputChange}
             placeholder="••••••••"
             className="input-bordered input-info input w-full"
             required
           />
         </div>
+
+        <div>
+          <label
+            htmlFor="repeat-password"
+            className="mb-2 block text-sm font-medium  "
+          >
+            {repeatPassword}
+          </label>
+          <input
+            type="password"
+            id="repeat-password"
+            name="registerPassword2"
+            value={registerPassword2}
+            onChange={onInputChange}
+            placeholder="••••••••"
+            className="input-bordered input-info input w-full"
+            required
+          />
+        </div>
+
         <button
           type="submit"
           className="btn-info btn w-full transition hover:brightness-110"
